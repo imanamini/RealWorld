cd209b99c64efa0f563f1623c438cd4e0596b86b
imanamini <iman.fa88@gmail.com>
2021-11-12 01:40:21 +0330

    add style global, fix #2

--- pages/register.vue
+++ pages/register.vue
@@ -1,10 +1,22 @@
 <template>
   <div class='container d-flex justify-content-center align-items-center vh-100'>
-    <div class='mx-auto bg-silver' style='width: 450px;padding: 35px 20px 20px 20px'>
-      <input v-model='email' class='form-control' placeholder='email'>
-      <input v-model='username' class='form-control' placeholder='username'>
-      <input v-model='password' class='form-control' placeholder='password'>
-      <button>submit</button>
+    <div class='register mx-auto bg-silver'>
+      <div class='text-center color-warm-grey fs-47 line-height-56 mb-4'>
+        Register
+      </div>
+      <form>
+          <label class='fs-16 color-charcoal-grey pl-2 mt-4'>User</label>
+          <input v-model='username' type='text' class='form-control' autocomplete='off'>
+          <label class='fs-16 color-charcoal-grey pl-2 mt-4'>Email</label>
+          <input v-model='email' type='email' class='form-control' autocomplete='off'>
+          <label class='fs-16 color-charcoal-grey pl-2 mt-4'>Password</label>
+          <input v-model='password' type='password' class='form-control' autocomplete='off'>
+          <input class='btn btn-primary w-100 mt-5' type="submit" value="Register">
+      </form>
+      <div class='fs-16 color-charcoal-grey line-height-19 mt-2 pt-1'>
+        <span>Already Registered?</span>
+        <span class='font-weight-bold'>Login</span>
+      </div>
     </div>
   </div>
 </template>
@@ -25,5 +37,8 @@ export default {
 </script>
 
 <style scoped>
-
+.register{
+  width: 450px;
+  padding: 35px 20px 20px 20px;
+}
 </style>
