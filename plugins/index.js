a6f28c82c9bf9c503255e0e54d9ef19e71e6b82f
imanamini <iman.fa88@gmail.com>
2021-11-12 02:37:15 +0330

    fix #6

--- plugins/index.js
+++ plugins/index.js
@@ -4,6 +4,20 @@ import vuex from 'vuex'
 const Cookie = process.client ? require('js-cookie') : undefined
 const tools = {
   $,
+  /**
+   * Set & get token
+   *
+   * When register/login we set token in localStorage, cookie & store
+   * and when we must check auth we get token from this function
+   *
+   * Author: Iman Amini
+   * last edit: 1400/08/21 by Iman Amini
+   * inspector:
+   *
+   * @param token is jwt that we set from api
+   * @return {string} is saved jwt that we get from storages
+   *
+   */
   conventionToken(token) {
     if (token !== undefined) {
       if (process.server) {
@@ -17,6 +31,42 @@ const tools = {
       return this.$store.state.token
     } else {
       return localStorage.getItem('token') || Cookie.get('token')
+    }
+  },
+  /**
+   * Check authenticated user
+   *
+   * Author: Iman Amini
+   * last edit: 1400/08/21 by Iman Amini
+   * inspector:
+   *
+   * @return {boolean}
+   */
+  isAuthenticated() {
+    return !!(tools.parseJwt() && tools.parseJwt().username)
+  },
+  /**
+   * Parse jwt
+   *
+   * Parse jwt to get username, image, bio & ...
+   *
+   * Author: Iman Amini
+   * last edit: 1400/08/21 by Iman Amini
+   * inspector:
+   *
+   */
+  parseJwt() {
+    const token = tools.conventionToken()
+    if (!token || !token.includes('.')) return {}
+    const base64Url = token.split('.')[1]
+    if (!base64Url) {
+      return ''
+    }
+    const base64 = base64Url.replace('-', '+').replace('_', '/')
+    try {
+      return JSON.parse(atob(base64))
+    } catch (e) {
+      tools.conventionToken('')
     }
   },
 }
