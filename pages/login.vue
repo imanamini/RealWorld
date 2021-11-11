a59626f1b36d92314f031b4ceba8d798fe6caee2
imanamini <iman.fa88@gmail.com>
2021-11-12 02:47:10 +0330

    fix #5

--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,7 @@
       <div class='text-center color-warm-grey fs-47 line-height-56 mb-4'>
         LOGIN
       </div>
-      <form>
+      <form @submit.prevent='login()'>
         <label class='fs-16 color-charcoal-grey pl-2 mt-4'>Email</label>
         <input v-model='email' type='email' class='form-control' autocomplete='off'>
         <label class='fs-16 color-charcoal-grey pl-2 mt-4'>Password</label>
@@ -28,6 +28,33 @@ export default {
       password:''
     }
   },
+  methods:{
+    /**
+     * Login user
+     *
+     * Author: Iman Amini
+     * last edit: 1400/08/21 by Iman Amini
+     * inspector:
+     *
+     */
+    login(){
+      this.$axios.post('users/login', {
+        user:{
+          email: this.email,
+          password: this.password
+        }
+      })
+        .then((response)=> {
+          if (response) {
+            this.conventionToken(response.data.user.token)
+            this.$router.push('/dashboard')
+          }
+        })
+        .catch((error)=> {
+          this.console(error);
+        });
+    }
+  }
 }
 </script>
 
