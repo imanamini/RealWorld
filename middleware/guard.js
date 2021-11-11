d8ddf7de4f7810e92c9a9141f03b36c834a7d1e3
imanamini <iman.fa88@gmail.com>
2021-11-12 02:53:31 +0330

    fix #7

--- middleware/guard.js
+++ middleware/guard.js
@@ -1,9 +1,19 @@
-// import { tools } from '~/plugins'
+import { tools } from '~/plugins'
 export default function (context) {
-  // if (context.route.fullPath === '/register' && tools.isAuthenticated()) {
-  //   context.redirect({ path: '/dashboard', status: 200 })
-  // }
-  // if (context.route.fullPath === '/' && tools.isAuthenticated()) {
-  //   context.redirect({ path: '/dashboard', status: 200 })
-  // }
+  if (context.route.fullPath !== '/login' && !tools.isAuthenticated()) {
+    context.redirect({ path: '/login', status: 200 })
+  }
+  if (context.route.fullPath === '/' && !tools.isAuthenticated()) {
+    context.redirect({ path: '/login', status: 200 })
+  }
+
+  if (context.route.fullPath === '/register' && tools.isAuthenticated()) {
+    context.redirect({ path: '/dashboard', status: 200 })
+  }
+  if (context.route.fullPath === '/' && tools.isAuthenticated()) {
+    context.redirect({ path: '/dashboard', status: 200 })
+  }
+  if (context.route.fullPath === '/login' && tools.isAuthenticated()) {
+    context.redirect({ path: '/dashboard', status: 200 })
+  }
 }
